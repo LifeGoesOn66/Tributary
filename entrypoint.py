c76744543b0c8a4d7847259d2705436a8b6a8af4
LifeGoesOn66 <1367094290@qq.com>
2024-07-15 21:26:43 +0100

    collect

--- entrypoint.py
+++ entrypoint.py
@@ -35,5 +35,24 @@ def record_engine_temperature():
 # practically identical to the above
 @app.route('/collect', methods=['POST'])
 def collect_engine_temperature():
-    return {"success": True}, 200
+    database=redis.Redis(host="redis",port=6379,db=0,decode_responses=True)
+
+    current_temperature=database.lindex(DATA_KEY,0)
+    logger.info(f"current engine temperature is :{current_temperature}")
+
+    engine_temperature_values=database.lrange(DATA_KEY,0,-1)
+    if engine_temperature_values:
+        engine_temperature_values=list(map(float,engine_temperature_values))
+        average_temperature=sum(engine_temperature_values)/len(engine_temperature_values)
+    else:
+        average_temperature=None
+
+    logger.info(f"average engine temperature is:{average_temperature}")
+
+    response_data={
+        "current_engine_temperature:":current_temperature,
+        "average_engine_temperture:":average_temperature
+    }
+
+    return json.dumps(response_data),200
 
